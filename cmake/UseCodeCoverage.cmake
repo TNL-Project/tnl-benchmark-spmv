1b9b14ad495131428adf819accc61eb473eccdc5
Jakub Klinkovský <klinkjak@fjfi.cvut.cz>
2017-03-25 21:44:04 +0100

    Made code coverage configurable and disabled by default

--- cmake/UseCodeCoverage.cmake
+++ cmake/UseCodeCoverage.cmake
@@ -1,31 +1,25 @@
 #http://www.cmake.org/pipermail/cmake/2010-March/036063.html
 
 
-OPTION( ENABLE_CODECOVERAGE "Enable code coverage testing support" )
+if ( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
+    message( WARNING "Code coverage results with an optimised (non-Debug) build may be misleading" )
+endif ( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
 
-if ( ENABLE_CODECOVERAGE )
+if ( NOT DEFINED CODECOV_OUTPUTFILE )
+    set( CODECOV_OUTPUTFILE cmake_coverage.output )
+endif ( NOT DEFINED CODECOV_OUTPUTFILE )
 
-    if ( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
-        message( WARNING "Code coverage results with an optimised (non-Debug) build may be misleading" )
-    endif ( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
+if ( NOT DEFINED CODECOV_HTMLOUTPUTDIR )
+    set( CODECOV_HTMLOUTPUTDIR coverage_results )
+endif ( NOT DEFINED CODECOV_HTMLOUTPUTDIR )
 
-    if ( NOT DEFINED CODECOV_OUTPUTFILE )
-        set( CODECOV_OUTPUTFILE cmake_coverage.output )
-    endif ( NOT DEFINED CODECOV_OUTPUTFILE )
-
-    if ( NOT DEFINED CODECOV_HTMLOUTPUTDIR )
-        set( CODECOV_HTMLOUTPUTDIR coverage_results )
-    endif ( NOT DEFINED CODECOV_HTMLOUTPUTDIR )
-
-    if ( CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUCXX )
-        find_program( CODECOV_GCOV gcov )
-        find_program( CODECOV_LCOV lcov )
-        find_program( CODECOV_GENHTML genhtml )
-        add_definitions( -fprofile-arcs -ftest-coverage )
-        link_libraries( gcov )
-        set( CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} --coverage )
-        add_custom_target( coverage_init ALL ${CODECOV_LCOV} --base-directory .  --directory ${CMAKE_SOURCE_DIR} --no-external --output-file ${CODECOV_OUTPUTFILE} --capture --initial --quiet )
-        add_custom_target( coverage ${CODECOV_LCOV} --base-directory .  --directory ${CMAKE_SOURCE_DIR} --no-external --output-file ${CODECOV_OUTPUTFILE} --capture --quiet COMMAND genhtml --quiet -o ${CODECOV_HTMLOUTPUTDIR} ${CODECOV_OUTPUTFILE} )
-    endif ( CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUCXX )
-
-endif (ENABLE_CODECOVERAGE )
+if ( CMAKE_COMPILER_IS_GNUCXX )
+    find_program( CODECOV_GCOV gcov )
+    find_program( CODECOV_LCOV lcov )
+    find_program( CODECOV_GENHTML genhtml )
+    add_definitions( -fprofile-arcs -ftest-coverage )
+    link_libraries( gcov )
+    set( CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} --coverage )
+    add_custom_target( coverage_init ALL ${CODECOV_LCOV} --base-directory .  --directory ${CMAKE_SOURCE_DIR} --no-external --output-file ${CODECOV_OUTPUTFILE} --capture --initial --quiet )
+    add_custom_target( coverage ${CODECOV_LCOV} --base-directory .  --directory ${CMAKE_SOURCE_DIR} --no-external --output-file ${CODECOV_OUTPUTFILE} --capture --quiet COMMAND genhtml --quiet -o ${CODECOV_HTMLOUTPUTDIR} ${CODECOV_OUTPUTFILE} )
+endif ( CMAKE_COMPILER_IS_GNUCXX )
