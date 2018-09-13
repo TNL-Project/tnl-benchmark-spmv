b009881cc8d3b6078d3e2a7bf1c85e0bb0bf9d77
Jakub Klinkovský <klinkjak@fjfi.cvut.cz>
2018-09-13 11:31:00 +0200

    Reorganization of benchmarks - moved into src

---
+++ src/tnl-benchmark-spmv.cu
@@ -0,0 +1,12 @@
+/***************************************************************************
+                          tnl-benchmark-spmv.cu  -  description
+                             -------------------
+    begin                : Jun 5, 2014
+    copyright            : (C) 2014 by Tomas Oberhuber
+    email                : tomas.oberhuber@fjfi.cvut.cz
+ ***************************************************************************/
+
+/* See Copyright Notice in tnl/Copyright */
+
+
+#include "tnl-benchmark-spmv.h"
