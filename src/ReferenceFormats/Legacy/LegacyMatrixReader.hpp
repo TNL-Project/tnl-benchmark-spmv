847ec56d6e7c1bc8b9b879e30e81d48e74cd58d0
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2023-03-16 09:49:28 +0100

    Fixed reading of integer matrices in legacy sparse matrix formats.

--- src/ReferenceFormats/Legacy/LegacyMatrixReader.hpp
+++ src/ReferenceFormats/Legacy/LegacyMatrixReader.hpp
@@ -151,8 +151,8 @@ bool LegacyMatrixReader< Matrix >::checkMtxHeader( const String& header,
    if( parsedLine[ 2 ] != "coordinates" &&
        parsedLine[ 2 ] != "coordinate" )
       throw std::runtime_error( std::string( "Error: Only 'coordinates' format is supported now, not " ) + parsedLine[ 2 ].getString() );
-   if( parsedLine[ 3 ] != "real" )
-      throw std::runtime_error( std::string( "Only 'real' matrices are supported, not " ) + parsedLine[ 3 ].getString() );
+   if( parsedLine[ 3 ] != "real" && parsedLine[ 3 ] != "integer" )
+      throw std::runtime_error( std::string( "Only 'real' and 'integer' matrices are supported, not " ) + parsedLine[ 3 ].getString() );
    if( parsedLine[ 4 ] != "general" )
    {
       if( parsedLine[ 4 ] == "symmetric" )
